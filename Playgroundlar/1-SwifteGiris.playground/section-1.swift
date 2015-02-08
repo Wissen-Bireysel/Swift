7f5279ff0699fa9f6e200a807533785951e5bbaa
Suleyman Calik <suleymancalik@gmail.com>
2015-02-08 09:08:36 +0200

    ilk proje eklendi

---
+++ Playgroundlar/1-SwifteGiris.playground/section-1.swift
@@ -0,0 +1,124 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var hosgeldinYazisi:String = "Hosgeldiniz!"
+
+var baskaYazi:Int = 123
+
+var sayi1 = 10
+var sayi2 = 20
+var sayi3 = sayi1 + sayi2
+
+var kesirli1 = 10.5
+var kesirli2 = 20.6
+
+var kesirli3 = kesirli1 + kesirli2
+
+var tamsayi:Int = Int(kesirli1 + kesirli2)
+
+tamsayi = 32
+tamsayi = 132
+tamsayi = 12345678900987654
+
+
+// tamsayi = "Merhaba"
+
+
+
+/*
+
+Burasi YORUM (Comment)
+
+*/
+
+
+//var baskaSayi:Int
+//baskaSayi
+
+
+
+let degismezSayi = 245
+// degismezSayi = 234567
+
+
+let swiftGuzelmi:Bool = true
+
+
+
+var sinifMevcudu:Int?
+sinifMevcudu
+sinifMevcudu = 15
+sinifMevcudu
+
+
+
+var sayilar:Array<Int> = Array<Int>()
+sayilar.append(100)
+sayilar.append(sayi1)
+sayilar.append(sayi2)
+sayilar.append(sayi3)
+
+
+sayilar[0]
+sayilar[1]
+sayilar[2]
+sayilar[3]
+//sayilar[4]
+
+if sayilar.count > 4 {
+    sayilar[4]
+}
+else {
+    println("Burasi calisti!")
+}
+
+
+sayilar.first
+sayilar.last
+
+sayilar.removeAll(keepCapacity: false)
+
+sayilar.first
+sayilar.last
+
+
+
+
+var farkliArray = [String]()
+
+farkliArray.append("A")
+farkliArray.append("B")
+farkliArray.append("C")
+farkliArray.append("D")
+farkliArray.append("E")
+
+
+farkliArray[0] = "Q"
+farkliArray
+//farkliArray[5] = "Z"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
