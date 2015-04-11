fd026f6de95900c5aa67c7e1460cd5f168dd9b07
Suleyman Calik <suleymancalik@gmail.com>
2015-04-11 11:13:18 +0300

    SizeClass projesi eklendi

---
+++ Projeler/13-SizeClass/SizeClass/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SizeClass
+//
+//  Created by Suleyman Calik on 11/04/15.
+//  Copyright (c) 2015 Wissen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    @IBAction func login(sender: UIButton) {
+    }
+    
+    @IBAction func forgotPassword(sender: UIButton) {
+    }
+
+
+}
+
