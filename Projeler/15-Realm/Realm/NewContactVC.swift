2a7a427faa421b65c0bd98e58496d242fb3adca4
Suleyman Calik <suleymancalik@gmail.com>
2015-04-12 12:49:53 +0300

    Realm projesi eklendi

---
+++ Projeler/15-Realm/Realm/NewContactVC.swift
@@ -0,0 +1,20 @@
+//
+//  NewContactVC.swift
+//  Realm
+//
+//  Created by Suleyman Calik on 12/04/15.
+//  Copyright (c) 2015 Wissen. All rights reserved.
+//
+
+import UIKit
+
+class NewContactVC: UIViewController {
+
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtNumber: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    @IBAction func saveContact(sender: UIButton) {
+    }
+
+}
